ad329910627ba3256e91d2fa8dfcdaf1788822c8
karkavelrajaj-coder <karkavelraja.j@ptuniv.edu.in>
2026-09-07 20:39:40 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -74,6 +74,7 @@ def ensure_indexes():
         (progress_col, [("user_id", 1), ("lesson_id", 1)], {"unique": True}),
         (submissions_col, [("assignment_id", 1), ("user_id", 1)], {}),
         (certificates_col, [("user_id", 1), ("course_id", 1)], {"unique": True}),
+        (certificates_col, "cert_id", {"unique": True}),
     ]
     for col_fn, keys, kwargs in index_specs:
         try:
